f0253f367989ba7ceca0e694be3a03c0c26a1771
Ambika <ambika121090@gmail.com>
2022-08-24 07:51:04 +0530

    Added required validation

--- CityWeatherDetails/src/app/weather-details/weather-details.component.ts
+++ CityWeatherDetails/src/app/weather-details/weather-details.component.ts
@@ -1,22 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherDetailsService } from '../services/weatherdetails.service';
 import { ICityWeatherDetails } from '../model/weather-details-info';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-weather-details',
   templateUrl: './weather-details.component.html',
   styleUrls: ['./weather-details.component.css']
 })
-// add pipe to format number to 2 or 3 digit decimal
+// TODO - add pipe to format number to 2 or 3 digit decimal
+
 export class WeatherDetailsComponent implements OnInit {
   city ='';
   showDetails = false;
   showTemp_F = false;
   cityweatherdetails: ICityWeatherDetails = {} as ICityWeatherDetails;
-  constructor(private weatherdetailservice: WeatherDetailsService) { }
+  form: FormGroup = this.formBuilder.group(
+    {
+      fullname: ['', Validators.required]
+    });
+  constructor(private weatherdetailservice: WeatherDetailsService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    
   }
+  /*
+  To get controls within formgroup
+  */
+  // get formControl(): { [key: string]: AbstractControl } {
+  //   return this.form.controls;
+  // }
 
   GetWeatherdetails(): void {
     if(this.city){
